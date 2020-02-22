98ca99fcf6d87a778347846a19a2126ebdcba553
Castell <pratikkatariya3.pk@gmail.com>
2020-02-22 01:32:13 -0500

    General - Permission Utils Files created to manages permission

--- app/src/main/java/com/example/pickle/SplashActivity.kt
+++ app/src/main/java/com/example/pickle/SplashActivity.kt
@@ -1,18 +1,45 @@
 package com.example.pickle
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
-import android.content.SharedPreferences
-import android.os.SharedMemory
-import com.google.android.gms.common.util.SharedPreferencesUtils
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val PERMISSION_ALL = 1
+
+        var permission = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.INTERNET,
+            Manifest.permission.READ_CONTACTS
+        )
+
+        if (!hasPermissions(this, permission)) {
+            ActivityCompat.requestPermissions(this, permission, PERMISSION_ALL)
+            return
+        }
+
+    }
+
+    override fun shouldShowRequestPermissionRationale(permission: String): Boolean {
+        return super.shouldShowRequestPermissionRationale(permission)
+    }
+
+    fun startNextActivity() {
         Handler().postDelayed({
 
             val sharedPref: SharedPreferences = getSharedPreferences("permissions", 0)
@@ -22,11 +49,14 @@ class SplashActivity : AppCompatActivity() {
                 sharedPref.edit().putBoolean("firstRun", false).apply()
                 finish()
             } else {
-                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
                 finish()
             }
 
         }, 2000)
+    }
 
+    fun hasPermissions(context: Context, permissions: Array<String>): Boolean = permissions.all {
+        ActivityCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
     }
 }
