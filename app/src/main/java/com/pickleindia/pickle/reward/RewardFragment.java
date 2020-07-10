d924b6365bffb09ce04a121df9c5c2085e7ecae4
pratikKataria <pratikkatariya3.pk@gmail.com>
2020-07-11 00:44:45 +0530

    RewardFragment.java - reward fragment created

---
+++ app/src/main/java/com/pickleindia/pickle/reward/RewardFragment.java
@@ -0,0 +1,21 @@
+package com.pickleindia.pickle.reward;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pickleindia.pickle.R;
+
+public class RewardFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reward, container, false);
+    }
+}
